6043cb6f22306f3b96e1ed6c26a2f21f18368e12
XMoSalahX <mohammedsalah6055@gmail.com>
2024-02-17 19:52:03 +0200

    Graceful shutdown.

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import { DocumentBuilder, SwaggerCustomOptions, SwaggerModule } from '@nestjs/sw
 import { AppOptions } from './utils/winston.helper';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as express from 'express';
-import { ValidationError, ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
+
   const app = await NestFactory.create<NestExpressApplication>(AppModule, AppOptions);
   app.use(express.json({ limit: '1mb' }));
   app.use(express.urlencoded({ extended: true, limit: '1mb' }));
@@ -42,5 +44,33 @@ async function bootstrap() {
   SwaggerModule.setup(process.env.SWAGGER_ROUTE, app, document, customOptions);
 
   await app.listen(process.env.SERVER_PORT);
+
+  // Graceful shutdown
+  async function gracefulShutdown(): Promise<void> {
+    const logger = new Logger('Shutdown');
+    logger.log('Starting graceful shutdown...');
+
+    try {
+      // Close any resources or connections here
+      // For example, closing the database connection
+      await app.close();
+      logger.log('Database connection closed');
+    } catch (error) {
+      logger.error(`Error closing database connection: ${error}`);
+    }
+
+    // Exiting the process
+    process.exit(0);
+  }
+
+  // Registering signal handlers
+  process.on('SIGINT', () => {
+    void gracefulShutdown();
+  }); // Ctrl+C
+  process.on('SIGTERM', () => {
+    void gracefulShutdown();
+  }); // Termination signal
+
+  logger.log('Application is listening on port 3000');
 }
 bootstrap();
