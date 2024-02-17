dfca375594e1dda1d7933384e75fafb7972c5fbd
XMoSalahX <mohammedsalah6055@gmail.com>
2024-02-17 18:16:27 +0200

    import { NestExpressApplication } from '@nestjs/platform-express';

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerCustomOptions, SwaggerModule } from '@nestjs/swagger';
+import { AppOptions } from './utils/winston.helper';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, AppOptions);
   // swagger config
   const options = new DocumentBuilder()
     .setTitle('Post Project')
