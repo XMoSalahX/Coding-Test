159107cde34967d60de8c86ec0b322e37b5595f8
XMoSalahX <mohammedsalah6055@gmail.com>
2024-02-17 17:56:31 +0200

    Swagger Configuration.

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerCustomOptions, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // swagger config
+  const options = new DocumentBuilder()
+    .setTitle('Post Project')
+    .setDescription('App For Code Test.')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const customOptions: SwaggerCustomOptions = {
+    swaggerOptions: {
+      persistAuthorization: true,
+      displayRequestDuration: true,
+    },
+  };
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document, customOptions);
+
+  await app.listen(3000);
+}
+bootstrap();
