57dbbd9b5bedfc18b94fd99b37a6d0097618e4bf
XMoSalahX <mohammedsalah6055@gmail.com>
2024-02-17 19:41:27 +0200

    config validation pipe.

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,26 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerCustomOptions, SwaggerModule } from '@nestjs/swagger';
 import { AppOptions } from './utils/winston.helper';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as express from 'express';
+import { ValidationError, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, AppOptions);
+  app.use(express.json({ limit: '1mb' }));
+  app.use(express.urlencoded({ extended: true, limit: '1mb' }));
+  app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
   // swagger config
   const options = new DocumentBuilder()
     .setTitle('Post Project')
